2dbeb95da0b68ceede028c6dddf1c63173adb28d
talootkhan09 <talootkhan09@gmail.com>
2024-01-30 02:12:28 +0500

    allowed hosts

--- ksainstrument/settings.py
+++ ksainstrument/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
